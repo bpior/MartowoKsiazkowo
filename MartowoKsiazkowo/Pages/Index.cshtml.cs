04569904c6a87cf798229c41b0c764d48a47eb35
bp <bartlomiejpiorczynski@gmail.com>
2023-07-27 14:48:32 +0200

    dalej ze skanerem

--- MartowoKsiazkowo/Pages/Index.cshtml.cs
+++ MartowoKsiazkowo/Pages/Index.cshtml.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.Common;
 
 namespace MartowoKsiazkowo.Pages;
 
@@ -10,8 +15,47 @@ private readonly IWebHostEnvironment _environment;
     public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment environment)
     {
         _logger = logger;
+        environment = _environment;
     }
 
+    [HttpPost]
+    public IActionResult Index (IFormCollection formCollection)
+    {
+        var writer = new QRCodeWriter();
+        var resultBit = writer.encode(formCollection["QRCodeString"], BarcodeFormat.QR_CODE, 200, 200);
+        var matrix = resultBit;
+        int scale = 2;
+        Bitmap restultBitmap = new Bitmap(matrix.Width * scale, matrix.Height * scale);
+        for (int x = 0; x < matrix.Height; x++)
+        {
+            for (int y = 0; y < matrix.Width; y++)
+            {
+                
+                Color pixel = matrix[x,y] ? Color.Black : Color.White;
+                for (int i = 0; i < scale; i++)
+                // {
+                //     
+                // }
+                    for (int j = 0; j < scale; j++)
+                // {
+                //     
+                // }
+                        restultBitmap.SetPixel(x * scale + i, y * scale + j, pixel);
+            }
+        }
+        
+        string webRootPath = _environment.WebRootPath;
+        restultBitmap.Save(webRootPath + "/Images/QRCodeNew.png");
+  //    ViewData[Url.ToString()] = "QRCodeNew.png";
+  //   ViewBag.Url = "QRCodeNew.png";
+  ViewData["URL"] = "/Images/QRCodeNew.png";
+  
+  
+     return Page();
+        
+//        return RedirectToPage("/Books/Index");
+    }
+    
     public void OnGet()
     {
     }
---
+++ MartowoKsiazkowo/Pages/Skaner.cshtml.cs
@@ -0,0 +1,147 @@
+/*
+using System.Drawing;
+using System.Drawing.Text;
+*/
+
+//using System.Drawing;
+/*
+using System.Drawing.Text;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QRCoder;
+using Image = SixLabors.ImageSharp.Image;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+*/
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using QRCoder;
+using System.IO;
+
+
+namespace MartowoKsiazkowo.Pages;
+
+public class Skaner : PageModel
+{
+    [BindProperty]
+    public string QRCodeText { get; set; }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!string.IsNullOrEmpty(QRCodeText))
+        {
+            // Generuj kod QR na podstawie wprowadzonego tekstu
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(QRCodeText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            BitmapByteQRCode qrCodeImage = new BitmapByteQRCode(qrCode);
+            byte[] qrCodeBytes = qrCodeImage.GetGraphic(20);
+
+            // Przekonwertuj dane obrazu na format ImageSharp
+            using (var image = Image.Load<Rgba32>(qrCodeBytes, out var format))
+            {
+                // Zapisz obraz jako plik PNG
+                var filePath = Path.Combine("wwwroot", "QRCode.png");
+                image.Save(filePath);
+            }
+
+            // Przekieruj użytkownika do strony z wygenerowanym kodem QR
+            return RedirectToPage("/QRCode");
+        }
+
+        return Page();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*[BindProperty]
+    public string QRCodeText { get; set; }
+
+    public IActionResult OnPost()
+    {
+        if (string.IsNullOrEmpty(QRCodeText))
+            return Page();
+
+        // Tworzenie kodu QR z podanego tekstu
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(QRCodeText, QRCodeGenerator.ECCLevel.Q);
+        //qrcode`
+        QRCode qrCode = new QRCode(qrCodeData);
+        BitmapByteQRCode qrCodeImage = new BitmapByteQRCode(qrCode);
+
+        // Wyświetlanie kodu QR jako obrazek na stronie
+        using (Image<Rgba32> qrImage = MediaTypeNames.Image.Load(qrCodeImage.GetGraphic(20)))
+        {
+            using (var graphics = new Image<Rgba32>(400, 400))
+            {
+                graphics.Mutate(ctx => ctx.Fill(Rgba32.White));
+                graphics.Mutate(ctx => ctx.DrawImage(qrImage, new SixLabors.ImageSharp.PointF(0, 0), 1));
+                graphics.Mutate(ctx => ctx.ApplyScalingWaterMark(CreateTextWatermark(QRCodeText, 20), Rgba32.Black, 1F, 1F));
+
+                using (var stream = new System.IO.MemoryStream())
+                {
+                    graphics.Save(stream, new SixLabors.ImageSharp.Formats.Png.PngEncoder());
+                    ViewData["QRCodeImage"] = $"data:image/png;base64,{Convert.ToBase64String(stream.ToArray())}";
+                }
+            }
+        }
+        */
+
+    //     return Page();
+    // }
+
+    /*private IImageProcessingContext CreateTextWatermark(string text, int fontSize)
+    {
+        var fontCollection = new FontCollection();
+        var font = fontCollection.Install("Arial.ttf");
+        var fontConfig = new TextGraphicsOptions(true)
+        {
+            WrapTextWidth = 300,
+            TextOptions = {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                TextStyle = new TextStyle()
+                {
+                    Font = new Font(font, fontSize, FontStyle.Regular),
+                    Color = Rgba32.Black,
+                    DpiX = 96,
+                    DpiY = 96
+                }
+            }
+        };
+
+        return new Image<Rgba32>(400, 400).DrawText(fontConfig, text);
+    }*/
+    
+    
+    
+    
+    
+}
